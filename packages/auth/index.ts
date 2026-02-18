a7cb572ef50b6889317f07efc6205c474cced6b5
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2026-02-18 20:05:56 +0100

    Refactor authentication options and remove providers

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -3,32 +3,7 @@ import type {
   NextApiRequest,
   NextApiResponse,
 } from "next";
-import { KyselyAdapter } from "@auth/kysely-adapter";
-import { getServerSession, type NextAuthOptions, type User } from "next-auth";
-import EmailProvider from "next-auth/providers/email";
-
-import { MagicLinkEmail, resend, siteConfig } from "@canvydocs/common";
-
-import { db } from "./db";
-import { env } from "./env.mjs";
-
-type UserId = string;
-type IsAdmin = boolean;
-
-declare module "next-auth" {
-  interface Session {
-    user: User & {
-      id: UserId;
-      isAdmin: IsAdmin;
-    };
-  }
-}
-
-declare module "next-auth" {
-  interface JWT {
-    isAdmin: IsAdmin;
-  }
-}
+import { getServerSession, type NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -37,91 +12,17 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
   },
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  adapter: KyselyAdapter(db),
-  providers: [
-    EmailProvider({
-      sendVerificationRequest: async ({ identifier, url }) => {
-        const user = await db
-          .selectFrom("User")
-          .select(["name", "emailVerified"])
-          .where("email", "=", identifier)
-          .executeTakeFirst();
-        const userVerified = !!user?.emailVerified;
-        const authSubject = userVerified
-          ? `Sign-in link for ${(siteConfig as { name: string }).name}`
-          : "Activate your account";
-
-        try {
-          await resend.emails.send({
-            from: env.RESEND_FROM,
-            to: identifier,
-            subject: authSubject,
-            react: MagicLinkEmail({
-              firstName: user?.name ?? "",
-              actionUrl: url,
-              mailType: userVerified ? "login" : "register",
-              siteName: (siteConfig as { name: string }).name,
-            }),
-            // Set this to prevent Gmail from threading emails.
-            // More info: https://resend.com/changelog/custom-email-headers
-            headers: {
-              "X-Entity-Ref-ID": new Date().getTime() + "",
-            },
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    }),
-  ],
+  providers: [],
   callbacks: {
-    session({ token, session }) {
-      if (token) {
-        if (session.user) {
-          session.user.id = token.id as string;
-          session.user.name = token.name;
-          session.user.email = token.email;
-          session.user.image = token.picture;
-          session.user.isAdmin = token.isAdmin as boolean;
-        }
-      }
+    session({ session }) {
       return session;
     },
-    async jwt({ token, user }) {
-      const email = token?.email ?? "";
-      const dbUser = await db
-        .selectFrom("User")
-        .where("email", "=", email)
-        .selectAll()
-        .executeTakeFirst();
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id;
-        }
-        return token;
-      }
-      let isAdmin = false;
-      if (env.ADMIN_EMAIL) {
-        const adminEmails = env.ADMIN_EMAIL.split(",");
-        if (email) {
-          isAdmin = adminEmails.includes(email);
-        }
-      }
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-        isAdmin: isAdmin,
-      };
+    async jwt({ token }) {
+      return token;
     },
   },
-  debug: env.IS_DEBUG === "true",
 };
 
-// Use it in server contexts
 export function auth(
   ...args:
     | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
@@ -132,6 +33,11 @@ export function auth(
 }
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions);
-  return session?.user;
+  return {
+    id: "4c153576-5ff9-4f35-a469-d8b6df1a2ff7",
+    name: "Emilien",
+    email: "emilien@etadam.com",
+    image: null,
+    isAdmin: true,
+  };
 }
